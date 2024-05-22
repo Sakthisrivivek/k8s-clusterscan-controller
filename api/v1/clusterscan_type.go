a3b3177c6fc20da523cc57ec052a5a11eb48fd19
Sakthisrivivek <90056891+Sakthisrivivek@users.noreply.github.com>
2024-05-22 16:06:19 -0400

    Update clusterscan_type.go

--- api/v1/clusterscan_type.go
+++ api/v1/clusterscan_type.go
@@ -1,24 +1,10 @@
 type ClusterScanSpec struct {
-	Schedule   string               `json:"schedule,omitempty"`
-	JobTemplate batchv1.JobTemplateSpec `json:"jobTemplate,omitempty"`
-  }
-  
-  type ClusterScanStatus struct {
-	LastRun string `json:"lastRun,omitempty"`
-	Status  string `json:"status,omitempty"`
-  }
-  
-  type ClusterScan struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Spec   ClusterScanSpec   `json:"spec,omitempty"`
-	Status ClusterScanStatus `json:"status,omitempty"`
-  }
-  
-  
-  type ClusterScanList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []ClusterScan `json:"items"`
-  }
-  
+    Schedule string json:"schedule,omitempty"
+    JobTemplate batchv1.JobSpec json:"jobTemplate"
+}
+
+type ClusterScanStatus struct {
+    LastRunTime *metav1.Time json:"lastRunTime,omitempty"
+    Conditions []metav1.Condition json:"conditions,omitempty"
+}
+
