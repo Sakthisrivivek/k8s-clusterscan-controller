cb26be9f6431a4ed19e0af93f6b2a7d677a84e04
Sakthisrivivek <90056891+Sakthisrivivek@users.noreply.github.com>
2024-05-22 16:39:55 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,101 +1,34 @@
-package main
-
 import (
-	"flag"
-	"net/http"
-	"os"
-	"time"
-
-	"github.com/Sakthisrivivek/k8s-clusterscan-controller/controllers"
-	"github.com/Sakthisrivivek/k8s-clusterscan-controller/pkg/generated/clientset/versioned"
-	"github.com/Sakthisrivivek/k8s-clusterscan-controller/pkg/generated/informers/externalversions"
-	"github.com/Sakthisrivivek/k8s-clusterscan-controller/pkg/signals"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
+    "os"
+    "sigs.k8s.io/controller-runtime"
+    "sigs.k8s.io/controller-runtime/pkg/log/zap"
+    "github.com/yourusername/clusterscan-operator/controllers"
+    "github.com/yourusername/clusterscan-operator/api/v1"
 )
-
-var (
-	masterURL  string
-	kubeconfig string
-)
-
-func init() {
-	flag.StringVar(&kubeconfig, "kubeconfig", "", "path to the kubeconfig file")
-	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
-	flag.Parse()
-}
 
 func main() {
-	logger := zap.New(zap.UseDevMode(true))
-	ctrl.SetLogger(logger)
-
-	cfg, err := rest.InClusterConfig()
-	if err != nil {
-		if kubeconfig != "" {
-			cfg, err = clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
-			if err != nil {
-				logger.Error(err, "Error building kubeconfig from flags")
-				os.Exit(1)
-			}
-		} else {
-			logger.Error(err, "Error building kubeconfig")
-			os.Exit(1)
-		}
-	}
-
-	mgr, err := manager.New(cfg, manager.Options{})
-	if err != nil {
-		logger.Error(err, "unable to create controller manager")
-		os.Exit(1)
-	}
-
-	kubeClient, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		logger.Error(err, "Error creating Kubernetes client")
-		os.Exit(1)
-	}
-
-	exampleClient, err := versioned.NewForConfig(cfg)
-	if err != nil {
-		logger.Error(err, "Error creating example client")
-		os.Exit(1)
-	}
-
-	exampleInformerFactory := externalversions.NewSharedInformerFactory(exampleClient, time.Second*30)
-
-
-	controller := controllers.NewClusterScanReconciler(
-		kubeClient,
-		exampleClient,
-		exampleInformerFactory.Example().V1().ClusterScans(),
-		logger.WithName("controllers").WithName("ClusterScan"),
-	)
-
-	
-	if err := controller.SetupWithManager(mgr); err != nil {
-		logger.Error(err, "unable to create controller", "controller", "ClusterScan")
-		os.Exit(1)
-	}
-
-	mux := http.NewServeMux()
-	mux.Handle("/healthz", healthz.Handler)
-
-	go func() {
-		addr := ":8081"
-		logger.Info("Starting healthz server", "addr", addr)
-		if err := http.ListenAndServe(addr, mux); err != nil {
-			logger.Error(err, "Failed to listen", "addr", addr)
-			os.Exit(1)
-		}
-	}()
-
-
-	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		logger.Error(err, "Problem running manager")
-		os.Exit(1)
-	}
+    ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+    mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+        Scheme:             scheme,
+        MetricsBindAddress: metricsAddr,
+        Port:               9443,
+        LeaderElection:     enableLeaderElection,
+        LeaderElectionID:   "clusterscan-operator",
+    })
+    if err != nil {
+        setupLog.Error(err, "unable to start manager")
+        os.Exit(1)
+    }
+    if err = (&controllers.ClusterScanReconciler{
+        Client: mgr.GetClient(),
+        Scheme: mgr.GetScheme(),
+    }).SetupWithManager(mgr); err != nil {
+        setupLog.Error(err, "unable to create controller", "controller", "ClusterScan")
+        os.Exit(1)
+    }
+    setupLog.Info("starting manager")
+    if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+        setupLog.Error(err, "problem running manager")
+        os.Exit(1)
+    }
 }
